ab393fc8d30fa5218f779dd41809a0ebb6389ff2
onceuponatim9 <sjung8018a@gmail.com>
2024-03-21 15:42:22 +0900

    Initial commit

---
+++ shop/src/shop/Main.java
@@ -0,0 +1,25 @@
+package shop;
+
+public class Main {
+	public static void main(String[] args) {
+		Shop shop = new Shop("Mega");
+		shop.run();
+		
+		// Shop Project
+		// class : Item, ItemManager, User, UserManager, 
+		
+		// method :
+		
+		// 유저 -
+		// ㄴ 회원가입/탈퇴/
+		// ㄴ 로그인/로그아웃/
+		// ㄴ 쇼핑하기/
+		// ㄴ 마이페이지(내장바구니, 항복삭제, 수량수정, 결제)/
+		// ㄴ 자동저장/자동로드
+		
+		// 관리자 -
+		// ㄴ 아이템등록/삭제/수정/
+		// ㄴ 조회(총 매출)
+	}
+
+}
