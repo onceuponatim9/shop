1d319e151b243f65853f5e2128a1886d301dc6e3
onceuponatim9 <sjung8018a@gmail.com>
2024-03-22 18:18:09 +0900

    Create FileManager

--- shop/src/shop/FileManager.java
+++ shop/src/shop/FileManager.java
@@ -1,8 +1,74 @@
 package shop;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class FileManager {
-	public void run() {
+	private UserManager um = new UserManager();
+	private ItemManager im = new ItemManager();
+	
+	private String fileName = "jang.txt";
+	private String userFileName = "user.txt";
+	private String ItemName = "item.txt";
+	
+	File userFile = new File(userFileName);
+	File jangFile = new File(fileName);
+	File itemFile = new File(ItemName);
+	
+	FileWriter fw = null;
+	FileReader fr = null;
+	BufferedReader br = null;
+	
+	private int count = 1;
+	private int itemCount = 0;
+	private int jangCount = 0;
+
+	
+	public FileManager() {
 		
+	}
+	
+	private void saveJangFile() {
+		
+	}
+	
+	private void saveUserFile() {
+		
+	}
+	
+	private void saveItemFile() {
+		
+	}
+	
+	private void loadJangFile() {
+		
+	}
+	
+	private void loadUserFile() {
+		
+	}
+	
+	private void loadItemFile() {
+		
+	}
+	
+	private void save() {
+		saveJangFile();
+		saveUserFile();
+		saveItemFile();
+	}
+	
+	private void load() {
+		loadJangFile();
+		loadUserFile();
+		loadItemFile();
+	}
+	
+	public void run() {
+		save();
+		load();
 	}
 
 }
